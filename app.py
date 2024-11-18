bce24d99543a412ca6c7978ef7b68821cb994ab3
Shruti Gupta <shrutigupta@Shrutis-MacBook-Air.local>
2024-11-19 00:52:50 +0530

    create ui

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, redirect, request, session, url_for, jsonify, render_template
+from flask_cors import CORS
+import requests
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)  # Secure the session
+CORS(app)  # This will allow all origins by default
+
+REDIRECT_URI = "http://localhost:5001/auth/github/callback"
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/get-started/')
+def getstarted():
+    return render_template('get-started.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
